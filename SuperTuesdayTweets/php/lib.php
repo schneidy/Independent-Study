61b38dd2015ce22d7d4e0b4701718a107b03d105
Dan Schneiderman <schneiderman.daniel@gmail.com>
2012-04-02 10:23:26 -0400

    Added max and min times scraped.

--- SuperTuesdayTweets/php/lib.php
+++ SuperTuesdayTweets/php/lib.php
@@ -290,7 +290,9 @@ if(isset($_GET['topic']) && !isset($_GET['count']) && isset($_GET['contains'])
 }
 
 
-// Earliest time
+// Earliest time scraped at
+// Kinda has a weird problem where it returns "created_at" when queried
+// Fixed with inclusion of searching for a space within the text of the tweet
 if(isset($_GET['topic']) && !isset($_GET['contains']) && !isset($_GET['count'])
 	&& !isset($_GET['time1']) && !isset($_GET['time2'])
 	&& isset($_GET['min']) && !isset($_GET['max'])
@@ -304,7 +306,7 @@ if(isset($_GET['topic']) && !isset($_GET['contains']) && !isset($_GET['count'])
 	mysql_select_db($db,$link) or die('Cannot select the DB');
 
 	/* grab the posts from the db */
-	$query = "SELECT MIN(created_at) as min FROM $topic";
+	$query = "SELECT min(scraped_at) as min FROM $topic where text like '%  %'";
     $result = mysql_query($query,$link) or die('Errant query:  '.$query);
 
 	/* create one master array of the records */
@@ -324,7 +326,7 @@ if(isset($_GET['topic']) && !isset($_GET['contains']) && !isset($_GET['count'])
 }
 
 
-// Latest time
+// Latest time scraped at
 if(isset($_GET['topic']) && !isset($_GET['contains']) && !isset($_GET['count'])
 	&& !isset($_GET['time1']) && !isset($_GET['time2'])
 	&& !isset($_GET['min']) && isset($_GET['max'])
@@ -338,7 +340,7 @@ if(isset($_GET['topic']) && !isset($_GET['contains']) && !isset($_GET['count'])
 	mysql_select_db($db,$link) or die('Cannot select the DB');
 
 	/* grab the posts from the db */
-	$query = "SELECT max(created_at) as max FROM $topic";
+	$query = "SELECT max(scraped_at) as max FROM $topic where text like '%  %'";
     $result = mysql_query($query,$link) or die('Errant query:  '.$query);
 
 	/* create one master array of the records */
@@ -357,5 +359,86 @@ if(isset($_GET['topic']) && !isset($_GET['contains']) && !isset($_GET['count'])
 	@mysql_close($link);
 }
 
+// Earliest time with phrase
+if(isset($_GET['topic']) && isset($_GET['contains']) && !isset($_GET['count'])
+	&& !isset($_GET['time1']) && !isset($_GET['time2'])
+	&& isset($_GET['min']) && !isset($_GET['max'])
+) {
+
+	/* soak in the passed variable or set our own */
+	$topic = $_GET['topic']; //no default
+	$contains = $_GET['contains'];
+
+	/* connect to the db */
+	$link = mysql_connect($server,$user,$pwd) or die('Cannot connect to the DB');
+	mysql_select_db($db,$link) or die('Cannot select the DB');
+
+	/* grab the posts from the db */
+	$query = "SELECT min(scraped_at) as min FROM $topic where text like '% $contains %'";
+    $result = mysql_query($query,$link) or die('Errant query:  '.$query);
+
+	/* create one master array of the records */
+	$tweets = array();
+	if(mysql_num_rows($result)) {
+		while($tweet = mysql_fetch_assoc($result)) {
+			$tweets[] = array('tweet'=>$tweet);
+		}
+	}
+
+	/* output in necessary format */
+    header('Content-type: application/json');
+	echo json_encode(array('tweets'=>$tweets));
+
+	/* disconnect from the db */
+	@mysql_close($link);
+}
+
+// Latest time with phrase
+if(isset($_GET['topic']) && isset($_GET['contains']) && !isset($_GET['count'])
+	&& !isset($_GET['time1']) && !isset($_GET['time2'])
+	&& !isset($_GET['min']) && isset($_GET['max'])
+) {
+
+	/* soak in the passed variable or set our own */
+	$topic = $_GET['topic']; //no default
+	$contains = $_GET['contains'];
+
+	/* connect to the db */
+	$link = mysql_connect($server,$user,$pwd) or die('Cannot connect to the DB');
+	mysql_select_db($db,$link) or die('Cannot select the DB');
+
+	/* grab the posts from the db */
+	$query = "SELECT max(scraped_at) as max FROM $topic where text like '% $contains %'";
+    $result = mysql_query($query,$link) or die('Errant query:  '.$query);
+
+	/* create one master array of the records */
+	$tweets = array();
+	if(mysql_num_rows($result)) {
+		while($tweet = mysql_fetch_assoc($result)) {
+			$tweets[] = array('tweet'=>$tweet);
+		}
+	}
+
+	/* output in necessary format */
+    header('Content-type: application/json');
+	echo json_encode(array('tweets'=>$tweets));
+
+	/* disconnect from the db */
+	@mysql_close($link);
+}
+
+// Names of tables
+
+// Tweets with coordinates
+
+// Tweets with phrase
+
+// Coordinates
+
+// Coordinates with phrase
+
+// Number of tweets with coordinates
+
+// Number of tweets with coordinates and phrase
 
 ?>
