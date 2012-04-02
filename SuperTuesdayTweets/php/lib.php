2622953e2f55bfbd82234cd7c0b24e4f8eca9606
Dan Schneiderman <schneiderman.daniel@gmail.com>
2012-04-02 01:36:35 -0400

    Added searches and counts between time marks.

--- SuperTuesdayTweets/php/lib.php
+++ SuperTuesdayTweets/php/lib.php
@@ -2,7 +2,9 @@
 include ("../../settings.php");
 
 // Basic Tweet Query
-if(isset($_GET['topic']) && !isset($_GET['contains']) && !isset($_GET['count'])) {
+if(isset($_GET['topic']) && !isset($_GET['contains']) && !isset($_GET['count'])
+	&& !isset($_GET['time1']) && !isset($_GET['time2'])
+) {
 
 	/* soak in the passed variable or set our own */
 	$number_of_tweets = isset($_GET['num']) ? intval($_GET['num']) : 10; //10 is the default
@@ -34,7 +36,9 @@ if(isset($_GET['topic']) && !isset($_GET['contains']) && !isset($_GET['count']))
 }
 
 // Tweet containing phrase
-if(isset($_GET['topic']) && isset($_GET['contains']) && !isset($_GET['count'])){
+if(isset($_GET['topic']) && isset($_GET['contains']) && !isset($_GET['count'])
+	&& !isset($_GET['time1']) && !isset($_GET['time2'])
+){
 
 	/* soak in the passed variable or set our own */
 	$number_of_tweets = isset($_GET['num']) ? intval($_GET['num']) : 10; //10 is the default
@@ -67,7 +71,9 @@ if(isset($_GET['topic']) && isset($_GET['contains']) && !isset($_GET['count'])){
 }
 
 // Number of tweets
-if(isset($_GET['topic']) && isset($_GET['count']) && !isset($_GET['contains'])) {
+if(isset($_GET['topic']) && isset($_GET['count']) && !isset($_GET['contains'])
+	&& !isset($_GET['time1']) && !isset($_GET['time2'])
+) {
 
 	// soak in the passed variable or set our own 
 	$topic = $_GET['topic']; //no default
@@ -97,7 +103,9 @@ if(isset($_GET['topic']) && isset($_GET['count']) && !isset($_GET['contains'])) 
 }
 
 // Number of tweets with phrase
-if(isset($_GET['topic']) && isset($_GET['contains']) && isset($_GET['count'])) {
+if(isset($_GET['topic']) && isset($_GET['contains']) && isset($_GET['count'])
+	&& !isset($_GET['time1']) && !isset($_GET['time2'])
+) {
 
 	// soak in the passed variable or set our own 
 	$topic = $_GET['topic']; //no default
@@ -127,5 +135,155 @@ if(isset($_GET['topic']) && isset($_GET['contains']) && isset($_GET['count'])) {
 	@mysql_close($link);
 }
 
+// Number of Tweets between time
+// -If searching for a specific minute, go between original minute and the following minute
+if(isset($_GET['topic']) && isset($_GET['count']) && !isset($_GET['contains'])
+	&& isset($_GET['time1']) && isset($_GET['time2'])
+	) {
+
+	// soak in the passed variable or set our own 
+	$topic = $_GET['topic']; //no default
+	$time1 = $_GET['time1'];
+	$time2 = $_GET['time2'];
+
+	/// connect to the db 
+	$link = mysql_connect($server,$user,$pwd) or die('Cannot connect to the DB');
+	mysql_select_db($db,$link) or die('Cannot select the DB');
+
+	// grab the posts from the db
+	$query = "SELECT count(*) FROM $topic where created_at between '$time1' and '$time2'";
+    $result = mysql_query($query,$link) or die('Errant query:  '.$query);
+
+	// create one master array of the records
+	$tweets = array();
+	if(mysql_num_rows($result)) {
+		while($tweet = mysql_fetch_assoc($result)) {
+			$tweets[] = array('tweet'=>$tweet);
+		}
+	}
+
+	// output in necessary format
+    header('Content-type: application/json');
+	echo json_encode(array('tweets'=>$tweets));
+
+	// disconnect from the db
+	@mysql_close($link);
+}
+
+// Number of Tweets between time containing phrase
+// -If searching for a specific minute, go between original minute and the following minute
+if(isset($_GET['topic']) && isset($_GET['count']) && isset($_GET['contains'])
+	&& isset($_GET['time1']) && isset($_GET['time2'])
+	) {
+
+	// soak in the passed variable or set our own 
+	$topic = $_GET['topic']; //no default
+	$time1 = $_GET['time1'];
+	$time2 = $_GET['time2'];
+	$contains = $_GET['contains'];
+
+	/// connect to the db 
+	$link = mysql_connect($server,$user,$pwd) or die('Cannot connect to the DB');
+	mysql_select_db($db,$link) or die('Cannot select the DB');
+
+	// grab the posts from the db
+	$query = "SELECT count(*) FROM $topic where text like '% $contains %' and created_at between '$time1' and '$time2'";
+    $result = mysql_query($query,$link) or die('Errant query:  '.$query);
+
+	// create one master array of the records
+	$tweets = array();
+	if(mysql_num_rows($result)) {
+		while($tweet = mysql_fetch_assoc($result)) {
+			$tweets[] = array('tweet'=>$tweet);
+		}
+	}
+
+	// output in necessary format
+    header('Content-type: application/json');
+	echo json_encode(array('tweets'=>$tweets));
+
+	// disconnect from the db
+	@mysql_close($link);
+}
+
+// Tweets between time
+// -If searching for a specific minute, go between original minute and the following minute
+if(isset($_GET['topic']) && !isset($_GET['count']) && !isset($_GET['contains'])
+	&& isset($_GET['time1']) && isset($_GET['time2'])
+	) {
+
+	// soak in the passed variable or set our own 
+	$topic = $_GET['topic']; //no default
+	$time1 = $_GET['time1'];
+	$time2 = $_GET['time2'];
+	$number_of_tweets = isset($_GET['num']) ? intval($_GET['num']) : 10; //10 is the default
+	$skip = isset($_GET['skip']) ? intval($_GET['skip']) : 0; // 0 is the default
+	
+	/// connect to the db 
+	$link = mysql_connect($server,$user,$pwd) or die('Cannot connect to the DB');
+	mysql_select_db($db,$link) or die('Cannot select the DB');
+
+	// grab the posts from the db
+	$query = "SELECT * FROM $topic where created_at between '$time1' and '$time2' LIMIT $skip, $number_of_tweets";
+    $result = mysql_query($query,$link) or die('Errant query:  '.$query);
+
+	// create one master array of the records
+	$tweets = array();
+	if(mysql_num_rows($result)) {
+		while($tweet = mysql_fetch_assoc($result)) {
+			$tweets[] = array('tweet'=>$tweet);
+		}
+	}
+
+	// output in necessary format
+    header('Content-type: application/json');
+	echo json_encode(array('tweets'=>$tweets));
+
+	// disconnect from the db
+	@mysql_close($link);
+}
+
+// Tweets between time with phrase
+// -If searching for a specific minute, go between original minute and the following minute
+if(isset($_GET['topic']) && !isset($_GET['count']) && isset($_GET['contains'])
+	&& isset($_GET['time1']) && isset($_GET['time2'])
+	) {
+
+	// soak in the passed variable or set our own 
+	$topic = $_GET['topic']; //no default
+	$time1 = $_GET['time1'];
+	$time2 = $_GET['time2'];
+	$contains = $_GET['contains'];
+	$number_of_tweets = isset($_GET['num']) ? intval($_GET['num']) : 10; //10 is the default
+	$skip = isset($_GET['skip']) ? intval($_GET['skip']) : 0; // 0 is the default
+
+	/// connect to the db 
+	$link = mysql_connect($server,$user,$pwd) or die('Cannot connect to the DB');
+	mysql_select_db($db,$link) or die('Cannot select the DB');
+
+	// grab the posts from the db
+	$query = "SELECT * FROM $topic where (created_at between '$time1' and '$time2') and (text like '% $contains %') LIMIT $skip, $number_of_tweets";
+    $result = mysql_query($query,$link) or die('Errant query:  '.$query);
+
+	// create one master array of the records
+	$tweets = array();
+	if(mysql_num_rows($result)) {
+		while($tweet = mysql_fetch_assoc($result)) {
+			$tweets[] = array('tweet'=>$tweet);
+		}
+	}
+
+	// output in necessary format
+    header('Content-type: application/json');
+	echo json_encode(array('tweets'=>$tweets));
+
+	// disconnect from the db
+	@mysql_close($link);
+}
+
+
+// Earliest time
+
+// Latest time
 
 ?>
