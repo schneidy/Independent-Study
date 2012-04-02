0ee8e71bb802fd028e1df7bd69c0c70e5e066bc7
Dan Schneiderman <schneiderman.daniel@gmail.com>
2012-04-01 23:43:11 -0400

    Added search for number of tweets with phrase.

--- SuperTuesdayTweets/php/lib.php
+++ SuperTuesdayTweets/php/lib.php
@@ -67,7 +67,7 @@ if(isset($_GET['topic']) && isset($_GET['contains']) && !isset($_GET['count'])){
 }
 
 // Number of tweets
-if(isset($_GET['topic']) && isset($_GET['count'])) {
+if(isset($_GET['topic']) && isset($_GET['count']) && !isset($_GET['contains'])) {
 
 	// soak in the passed variable or set our own 
 	$topic = $_GET['topic']; //no default
@@ -96,6 +96,36 @@ if(isset($_GET['topic']) && isset($_GET['count'])) {
 	@mysql_close($link);
 }
 
+// Number of tweets with phrase
+if(isset($_GET['topic']) && isset($_GET['contains']) && isset($_GET['count'])) {
+
+	// soak in the passed variable or set our own 
+	$topic = $_GET['topic']; //no default
+	$contains = $_GET['contains'];
+
+	/// connect to the db 
+	$link = mysql_connect($server,$user,$pwd) or die('Cannot connect to the DB');
+	mysql_select_db($db,$link) or die('Cannot select the DB');
+
+	// grab the posts from the db
+	$query = "SELECT count(*) FROM $topic where text like '% $contains %'";
+    $result = mysql_query($query,$link) or die('Errant query:  '.$query);
+
+	// create one master array of the records
+	$tweets = array();
+	if(mysql_num_rows($result)) {
+		while($tweet = mysql_fetch_assoc($result)) {
+			$tweets[] = array('tweet'=>$tweet);
+		}
+	}
+
+	// output in necessary format
+    header('Content-type: application/json');
+	echo json_encode(array('tweets'=>$tweets));
+
+	// disconnect from the db
+	@mysql_close($link);
+}
 
 
 ?>
