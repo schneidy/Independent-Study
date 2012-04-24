8c152f4db143d011b19b8ecd6f7af1ec7cd6ea2c
Dan Schneiderman <schneiderman.daniel@gmail.com>
2012-04-24 01:37:29 -0400

    Initial Commit.

---
+++ OpenStatesHeatMap/php/proxy.php
@@ -0,0 +1,15 @@
+<?php
+	// API key to be removed
+	$apikey = "68170bb9e8c64913b1dc0e58df47d822";
+	
+	// Sets the page to act as a json file
+	header('Content-type: application/json');
+	
+	if(isset($_GET['topic']) && isset($_GET['state']) ){
+		$topic = $_GET['topic'];
+		$state =  $_GET['state'];
+		$url = "http://openstates.org/api/v1/bills/?q=".$topic."&state=".$state."&apikey=".$apikey;
+		$json = file_get_contents($url);
+		echo $json;
+	}
+?>
