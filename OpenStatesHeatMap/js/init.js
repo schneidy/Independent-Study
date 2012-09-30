ab33b4c69cea2a20d0e293f076764aee19f2b881
Dan Schneiderman <schneiderman.daniel@gmail.com>
2012-09-30 19:32:21 -0400

    Added gradient bar and special case for scrapping data.

--- OpenStatesHeatMap/js/init.js
+++ OpenStatesHeatMap/js/init.js
@@ -43,16 +43,58 @@ function overallInit(){
         .style("z-index", "10")
         .style("visibility", "hidden");
 
+    // Gradient Bar
+    rangeBar = svg.append('svg:rect')
+        .attr('id', 'rangeOfBills')
+        .attr('x',  400)
+        .attr('y', 17)
+        .attr('width', 200)
+        .attr('height', 25)
+        .style('fill', 'white');
+
+    var gradient = d3.select("#mainSVG").append("svg:defs")
+        .append("svg:linearGradient")
+            .attr("id", "gradient")
+            .attr("x1", "0%")
+            .attr("y1", "0%")
+            .attr("x2", "100%")
+            .attr("y2", "100%")
+            .attr("spreadMethod", "pad");
+
+    gradient.append("svg:stop")
+        .attr("offset", "0%")
+        .attr("stop-color", "lightgreen")
+        .attr("stop-opacity", 1);
+
+    gradient.append("svg:stop")
+        .attr("offset", "100%")
+        .attr("stop-color", "darkblue")
+        .attr("stop-opacity", 1);
+
+    svg.append("svg:text")
+        .attr("id", "rangeMin")
+        .attr("x", 400)
+        .attr("y", 15);
+    svg.append("svg:text")
+        .attr("id", "rangeMax")
+        .attr("x", 580)
+        .attr("y", 15);
+
+
 };
 
 // Topic Selection and coloring
 function topicSelection(topic, topicMethod){
     
+    d3.select("#bills").selectAll("h3").remove();
+    d3.select("#bills").select("ul").remove();
+    d3.select('#bills').append("h3").text("Searched for " + topic);
+
     var cleanTopic = topic.replace(/\s/g, '').replace(/\,/g,"");
     var url = "../json/totalBills/totalBills-" + cleanTopic + '.json';
     d3.json(url, function(json){
         data = json;
-        fill = d3.scale.linear().domain([data.smallest_amount_bills, data.largest_amount_bills]).range(["lightgreen", "darkgreen"]);
+        fill = d3.scale.linear().domain([data.smallest_amount_bills, data.largest_amount_bills]).range(["lightgreen", "darkblue"]);
         tooltip = d3.select("#tooltip");
 
         d3.select(states).selectAll("path")
@@ -61,7 +103,7 @@ function topicSelection(topic, topicMethod){
                 var state_name = d.properties.name.replace( /([a-z])([A-Z])/, "$1 $2").replace( /([a-z])([A-Z])/, "$1 $2");
                 var abbr = stateAbbr[state_name];
                 return fill(data[abbr].totalBills);
-                }).each(function(state){
+            }).each(function(state){
 
                 d3.select(this).on("mouseover", function(d){
                     tooltip.selectAll("p").remove();
@@ -71,8 +113,13 @@ function topicSelection(topic, topicMethod){
                     })
                     .on("mousemove", function(){return d3.select("#tooltip").style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
                     .on("mouseout", function(){return d3.select("#tooltip").style("visibility", "hidden");})
-                    .on("mousedown", function(){displayBills(stateAbbr[state.properties.name], topic)});
+                    .on("mousedown", function(){displayBills(stateAbbr[state.properties.name], topic, state.properties.name)});
             });
+    
+        // Gradient Bar
+        d3.select("#rangeOfBills").style("fill", "url(#gradient)");
+        d3.select("#rangeMin").text(data.smallest_amount_bills);
+        d3.select("#rangeMax").text(data.largest_amount_bills)
     });
 
 };
@@ -80,23 +127,32 @@ function topicSelection(topic, topicMethod){
 
 
 // Displays the most recent bills on the subject (10 max)
-function displayBills(stateID, topic){
+function displayBills(stateID, topic, state){
 
     //clears bills if there are any.
-    d3.select("#bills").selectAll("p").remove()
     d3.select("#bills").selectAll("h3").remove();
-    
+    d3.select("#bills").select("ul").remove();
+
     // Most recent bills
     var recent_bills = data[stateID].most_recent_bills;
 
     d3.select('#bills').append("h3")
-        .text(recent_bills.length > 0 ? recent_bills.length + " recent bills found on " + topic : "There are no recent bills found on " + topic);
+        .text(function(){
+            retMsg = recent_bills.length > 0 ? recent_bills.length + " recent bills found" : "There are no recent bills found";
+            return retMsg + ' on ' + topic + ' in ' + state;
+            });
    
+    d3.select("#bills").append("ul");
     recent_bills.forEach(function(bill){
-        d3.select("#bills").append("p")
-            .text(bill.bill_id)
-            .on("mouseover", function(){d3.select(this).text(bill.bill_id + ' - ' + bill.title);})
-            .on("mouseout", function(){d3.select(this).text(bill.bill_id)})
+        var bill_li = d3.select("#bills").select("ul").append("li");
+            
+        bill_li.insert("a")
+            .attr("href", bill.url)
+            .text(bill.bill_id);
+            
+        bill_li.insert("p").text(bill.title.substring(0, Math.max(bill.title.length / 3, 25) ) + "...")
+            .on("mouseover", function(){d3.select(this).text(bill.title);})
+            .on("mouseout", function(){d3.select(this).text(bill.title.substring(0, bill.title.length / 3) + "...")})
     });
     
 }
